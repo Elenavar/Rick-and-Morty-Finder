671c59cdb13bbb2dd9d86d2a95469a84f8f0053a
Elena <elenavargasm9@gmail.com>
2022-12-13 10:54:30 +0100

    add html

--- src/components/FilterBySpecie.js
+++ src/components/FilterBySpecie.js
@@ -1,5 +1,19 @@
+import '../styles/components/FilterBySpecie.scss';
 function FilterBySpecie() {
-  return <label>Specie:</label>;
+  return (
+    <>
+      <label className="specie" htmlFor="specie">
+        Specie:
+      </label>
+      <select className="select" id="specie">
+        <option value="all" selected>
+          All
+        </option>
+        <option value="human">Human</option>
+        <option value="alien">Alien</option>
+      </select>
+    </>
+  );
 }
 
 export default FilterBySpecie;
